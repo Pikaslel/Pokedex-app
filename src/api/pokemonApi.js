eb7c0d6c961d7d7967fd111eb1a7f380bd390833
Daniel Felipe Matiz <felipe.matiz404@gmail.com>
2026-02-11 18:58:27 -0500

    Estructura carpetas y fetch api pokemon

---
+++ src/api/pokemonApi.js
@@ -0,0 +1,13 @@
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+export const getPokemons = async () => {
+    const response = await fetch(`${BASE_URL}/pokemon?limit=151`);
+    const data = await response.json();
+
+    return data.results.sort((a, b) => a.name.localeCompare(b.name));
+};
+
+export const getPokemonByName = async (name) => {
+    const response = await fetch(`${BASE_URL}/pokemon/${name}`);
+    return await response.json();
+};
